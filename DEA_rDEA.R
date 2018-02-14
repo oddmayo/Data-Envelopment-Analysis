208aefd099372e57e5ef03e0e87a523778b4447e
Camilo <camayorquina@unal.edu.co>
2018-02-14 14:25:35 -0500

    Puliendo código

--- DEA_rDEA.R
+++ DEA_rDEA.R
@@ -1,16 +1,18 @@
 # DEA rama judicial paquete rDEA
 
+# Cargar librerías
 library(rDEA)
 library(readxl)
 
 
 
-#Load data
+# Cargar datos en su totalidad
 data <- read_excel("C:/Users/CamiloAndrés/Desktop/DNP/Proyectos/Distribución de la oferta judicial/Base filtrada/Base esta si es.xlsx")
-# Datos especialidad civil
+# Cargar datos solo espcialidad civil
 data <- read_excel("C:/Users/CamiloAndrés/Desktop/DNP/Proyectos/Distribución de la oferta judicial/DEA en R/BaseCivil.xlsx")
 
 
+# dataframe para la función dea
 inputdata <- data.frame(data[3])
 outputdata <- data.frame(data[2])
 
@@ -19,12 +21,12 @@ input <- as.matrix(inputdata)
 output <- as.matrix(outputdata)
 
 
-# Paquete rDEA
 # Test de rendimientos a escala
 rendimientos <- rts.test(input,output,model="output",H0="constant",bw="cv", B=100,alpha = 0.05)
 rendimientos$pvalue
 
 
+# Eficiencias feo
 e <- dea(XREF=input,YREF=output,input,output, RTS = "variable", model = "output")
 e
 summary(e)
