0e0041e8709b377edeb09a60a459362d29e7260a
Camilo <camayorquina@unal.edu.co>
2018-03-12 11:15:46 -0500

    Orden definitivo del código

--- DEA.R
+++ DEA.R
@@ -3,7 +3,6 @@
 ####################################
 
 library(readxl) 
-library(Benchmarking) #paquete del libro guía
 
 #######################################################
 # JUZGADOS proceso reparación directa entre CIRCUITOS #
@@ -14,15 +13,39 @@ inputs1 <- data.frame(c(juzgcirc[5],juzgcirc[6])) #vector de entradas
 inputs1 <- as.matrix(inputs1) #matriz necesaria para la función dea de Benchmarking
 outputs1 <- data.frame(juzgcirc[3]) #vector de salidas
 outputs1 <- as.matrix(outputs1)
+library(rDEA) #paquete con test de rendimientos a escala
+# test de rendimientos a escala
+rendimientos1 <- rts.test(inputs1,outputs1,model="output",H0="constant",bw="cv", B=100,alpha = 0.05)
+rendimientos1$pvalue #si es mayor a 0.05, los rendimientos son variables
+detach(package:rDEA)
+
+# detección de DATOS ATÍPICOS con nube de datos
+x1 <- with(juzgcirc, cbind(juzgcirc$Jueces))
+y1 <- with(juzgcirc, cbind(juzgcirc$Carga))
+w1 <- with(juzgcirc, cbind(juzgcirc$Egresos))
+x1y1w1 <- cbind(x1,y1,w1)
+D1 <- det(t(x1y1w1)%*%x1y1w1)
+i1 <- c(1) #número de DMUs a quitarse, que se sospeche que sean atípicos: En este caso, Bogotá
+x1y1w1i1 = x1y1w1[-i1,]
+D1i <- det( t(x1y1w1i1) %*% x1y1w1i1 )
+R1i <- D1i/D1
+R1i #valores pequeños de Ri me indican presencia de datos atípicos
+library(Benchmarking) #paquete con funciones dea
+# GRÁFICAMENTE, entre más DMUs se quiten, más tarda la gráfica en generarse
+atipico1 <- outlier.ap(inputs1,outputs1,NDEL=5) #proceso lento
+# para que se sospeche de un dato atípico, la distancia entre el punto ubicado en cero y la línea punteada tiene que ser muy grande
+outlier.ap.plot(atipico1$ratio)
+
 dea1 <- dea(inputs1,outputs1,RTS="vrs",ORIENTATION = "out",SLACK = TRUE, DUAL = TRUE) #rendimientos variables, orientado a salidas y 2 etapas
-# eficiencia técnica
+# EFICIENCIA técnica 
 eftcircj <- 1/dea1$eff 
 eftcircj
 eft_circj <- data.frame(Circuito=juzgcirc$CIRCUITO,Eficiencia_técnica=eftcircj)
-eft_circj
-# unidades de referencia
+eft_circj_ordenada <- eft_circj[order(-eft_circj$Eficiencia_técnica),] # ordenar por eficiencia
+eft_circj_ordenada
+# unidades de REFERENCIA
 unidades1 <- data.frame(dea1$lambda)
-# recomendación
+# RECOMENDACIÓN
 recomen1 <- data.frame(Circuito=juzgcirc$CIRCUITO,Bogotá=unidades1$L5,
                       Leticia=unidades1$L16,
                       Mocoa=unidades1$L19,
@@ -30,23 +53,23 @@ recomen1 <- data.frame(Circuito=juzgcirc$CIRCUITO,Bogotá=unidades1$L5,
                       Pasto=unidades1$L23,
                       Eficiencia_técnica=eftcircj,
                       "Aumento_recomendado_en_porcentaje"=(1-eftcircj)*100)
-recomen1
-# holguras (eficiencia fuerte o débil)
+recomen1_ordenada <- recomen1[order(-eft_circj$Eficiencia_técnica),]
+recomen1_ordenada
+# HOLGURAS (eficiencia fuerte o débil)
 dea1$sx
 dea1$sy
-# pesos (sensibilidad/impacto sobre la eficiencia)
+# PESOS (sensibilidad/impacto sobre la eficiencia)
 dea1$ux
-dea1$vy
+dea1$vy 
 
-
-# supereficiencia (Para detectar valores atípico/Diferenciar unidades frontera)
+# SUPEREFICIENCIA (Para detectar valores atípico/Diferenciar unidades frontera)
 superdea1 <- sdea(inputs1,outputs1,RTS = "vrs",ORIENTATION = "out")
 1/superdea1$eff
 superef1 <- data.frame(Circuito=juzgcirc$CIRCUITO,Super_eficiencia=1/superdea1$eff)
 superef1
-
+superef1_ordenada <- superef1[order(-superef1$Super_eficiencia),]
+superef1_ordenada
 superdea1$lambda
-
 
 #######################################################
 # JUZGADOS proceso reparación directa entre DISTRITOS #
@@ -57,27 +80,54 @@ inputs2 <- data.frame(c(juzgdist[5],juzgdist[6])) #vector de entradas
 inputs2 <- as.matrix(inputs2) #matriz necesaria para la función dea de Benchmarking
 outputs2 <- data.frame(juzgdist[3]) #vector de salidas
 outputs2 <- as.matrix(outputs2)
+
+detach(package:Benchmarking)
+library(rDEA) #paquete con test de rendimientos a escala
+# test de rendimientos a escala
+rendimientos2 <- rts.test(inputs2,outputs2,model="output",H0="constant",bw="cv", B=100,alpha = 0.05)
+rendimientos2$pvalue #si es mayor a 0.05, los rendimientos son variables
+detach(package:rDEA)
+library(Benchmarking)
+
+# detección de DATOS ATÍPICOS con nube de datos
+x2 <- with(juzgdist, cbind(juzgdist$Jueces))
+y2 <- with(juzgdist, cbind(juzgdist$Carga))
+w2 <- with(juzgdist, cbind(juzgdist$Egresos))
+x2y2w2 <- cbind(x2,y2,w2)
+D2 <- det(t(x2y2w2)%*%x2y2w2)
+i2 <- c(1) #número de  DMUs a quitarse, que se sospeche que sean atípicos
+x2y2w2i2 = x2y2w2[-i2,]
+D2i <- det( t(x2y2w2i2) %*% x2y2w2i2 )
+R2i <- D2i/D2
+R2i #valores pequeños de Ri me indican presencia de datos atípicos
+
+# GRÁFICAMENTE, entre más DMUs se quiten, más tarda la gráfica en generarse
+atipico2 <- outlier.ap(inputs2,outputs2,NDEL=5)
+# para que se sospeche de un atípico, la distancia entre el punto ubicado en cero y la línea punteada tiene que ser muy grande
+outlier.ap.plot(atipico2$ratio)
+
 dea2 <- dea(inputs2,outputs2,RTS="vrs",ORIENTATION = "out",SLACK = TRUE, DUAL = TRUE) #rendimientos variables, orientado a salidas y 2 etapas
-# eficiencia técnica
+
+# EFICIENCIA técnica
 eftdistj <- 1/dea2$eff 
 eftdistj
 eft_distj <- data.frame(Circuito=juzgdist$DISTRITO,Eficiencia_técnica=eftdistj)
 eft_distj
-# unidades de referencia
+eft_distj_ordenada <- eft_distj[order(-eft_distj$Eficiencia_técnica),] #ordenar por eficiencia
+eft_distj_ordenada
+# unidades de REFERENCIA
 dea2$lambda
-# holguras (eficiencia fuerte o débil)
+# HOLGURAS (eficiencia fuerte o débil)
 dea2$sx
 dea2$sy
 
-# supereficiencia (para detectar valores atípico/diferenciar unidades frontera)
+# SUPEREFICIENCIA (para detectar valores atípico/diferenciar unidades frontera)
 superdea2 <- sdea(inputs2,outputs2,RTS = "vrs",ORIENTATION = "out")
 1/superdea2$eff
 superef2 <- data.frame(Distrito=juzgdist$DISTRITO,Super_eficiencia=1/superdea2$eff)
 superef2
 
 superdea2$lambda
-
-
 
 #########################################################
 # TRIBUNALES proceso reparación directa entre DISTRITOS #
@@ -88,15 +138,44 @@ inputs3 <- data.frame(c(tribdist[5],tribdist[6])) #vector de entradas
 inputs3 <- as.matrix(inputs3) #matriz necesaria para la función dea de Benchmarking
 outputs3 <- data.frame(tribdist[3]) #vector de salidas
 outputs3 <- as.matrix(outputs3)
+
+detach(package:Benchmarking)
+library(rDEA) #paquete con test de rendimientos a escala
+# test de rendimientos a escala
+rendimientos3 <- rts.test(inputs3,outputs3,model="output",H0="constant",bw="cv", B=100,alpha = 0.05)
+rendimientos3$pvalue #si es mayor a 0.05, los rendimientos son variables
+detach(package:rDEA)
+library(Benchmarking)
+
+# detección de DATOS ATÍPICOS con nube de datos
+x3 <- with(tribdist, cbind(tribdist$Jueces))
+y3 <- with(tribdist, cbind(tribdist$Carga))
+w3 <- with(tribdist, cbind(tribdist$Egresos))
+x3y3w3 <- cbind(x3,y3,w3)
+D3 <- det(t(x3y3w3)%*%x3y3w3)
+i3 <- c(1) # DMUs a quitarse, que se sospeche que sean atípicos
+x3y3w3i3 = x3y3w3[-i3,]
+D3i <- det( t(x3y3w3i3) %*% x3y3w3i3 )
+R3i <- D3i/D3
+R3i # Valores pequeños de Ri me indican presencia de datos atípicos
+
+# GRÁFICAMENTE, entre más DMUs se quiten, más tarda la gráfica en generarse
+atipico3 <- outlier.ap(inputs3,outputs3,NDEL=5)
+# para que se sospeche de un dato atípico, la distancia entre el punto ubicado en cero y la línea punteada tiene que ser muy grande
+outlier.ap.plot(atipico3$ratio)
+
 dea3 <- dea(inputs3,outputs3,RTS="vrs",ORIENTATION = "out",SLACK = TRUE, DUAL = TRUE) #rendimientos variables, orientado a salidas y 2 etapas
-# eficiencia técnica
+
+# EFICIENCIA técnica
 eftdistt <- 1/dea3$eff 
 eftdistt
 eft_distt <- data.frame(Distrito=tribdist$DISTRITO,Eficiencia_técnica=eftdistt)
 eft_distt
-# unidades de referencia
+eft_distt_ordenada <- eft_distt[order(-eft_distt$Eficiencia_técnica),] #ordenar por eficiencia
+eft_distt_ordenada
+# unidades de REFERENCIA
 unidades3 <- data.frame(dea3$lambda)
-# recomendación
+# RECOMENDACIÓN
 recomen3 <- data.frame(Distrito=tribdist$DISTRITO,Arauca=unidades3$L2,
                        Cundinamarca=unidades3$L13,
                        San_Andrés=unidades3$L22,
@@ -107,7 +186,7 @@ recomen3
 # holguras (evidencia eficiencia fuerte o débil)
 dea3$sx
 dea3$sy
-# supereficiencia (para detectar valores atípico/diferenciar unidades frontera)
+# SUPEREFICIENCIA (para detectar valores atípico/diferenciar unidades frontera)
 superdea3 <- sdea(inputs3,outputs3,RTS = "vrs",ORIENTATION = "out")
 1/superdea3$eff
 superef3 <- data.frame(Distrito=tribdist$DISTRITO,Super_eficiencia=1/superdea3$eff)
@@ -115,14 +194,19 @@ superef3
 
 superdea3$lambda
 
+# COMPARACIÓN entre primera y segunda instancia
+comp <- data.frame(Distrito=tribdist$DISTRITO,eficiencia_juzgados=eftdistj,eficiencia_tribunales=eftdistt)
+comp
 
 ##########################
 ## EXPORTAR eficiencias ##
 ##########################
 library(xlsx)
-write.xlsx(eft_circj,"C:/Users/CamiloAndrés/Desktop/DNP/basejc.xlsx")
-write.xlsx(eft_distj,"C:/Users/CamiloAndrés/Desktop/DNP/basejd.xlsx")
-write.xlsx(eft_distt,"C:/Users/CamiloAndrés/Desktop/DNP/basetd.xlsx")
+write.xlsx(eft_circj_ordenada,"C:/Users/CamiloAndrés/Desktop/basejc_ordenada.xlsx")
+write.xlsx(eft_distj_ordenada,"C:/Users/CamiloAndrés/Desktop/basejd_ordenada.xlsx")
+write.xlsx(eft_distt_ordenada,"C:/Users/CamiloAndrés/Desktop/basetd_ordenada.xlsx")
+write.xlsx(recomen1_ordenada,"C:/Users/CamiloAndrés/Desktop/recomen1_ordenada.xlsx")
+write.xlsx(superef1_ordenada,"C:/Users/CamiloAndrés/Desktop/superef1_ordenada.xlsx")
 
 ###########################################################################
 ## GRÁFICOS DE FRONTERAS DE PRODUCCIÓN (2 inputs para producir 1 output) ##
@@ -135,17 +219,17 @@ library(ggrepel)
 #########################################
 
 ########### Gráfico con función del paquete para saber forma de la frontera
-inputnj_1 <- as.matrix(inputnj_1)
-inputct_1 <- as.matrix(inputct_1)
-plotef <- dea.plot.frontier(inputnj_1,inputct_1,RTS = "vrs")
+#inputs1 <- as.matrix(inputs1)
+#outputs11 <- as.matrix(outputs1)
+#plotef <- dea.plot.frontier(inputs1,outputs1,RTS = "vrs")
 ###########
 
 tablaf1 <- data.frame(juzgcirc,eficiencia=dea1$eff)
-myColors <- c("firebrick4", "slateblue4")
+myColors <- c("firebrick4", "slateblue4") #colores de eficiencia e ineficiencia
 tablalabelf1 <- data.frame(juzgcirc,eficiencia=dea1$eff)
 tablalabelf1$eficiencia[-which(tablalabelf1$eficiencia==1)]=0
 tablalabelf1$color=rep("firebrick4",length(tablalabelf1$CIRCUITO))
-tablalabelf1$color[which(tablalabelf1$eficiencia==1)]=rep("slateblue4", 5)
+tablalabelf1$color[which(tablalabelf1$eficiencia==1)]=rep("slateblue4", 5) #número de unidades eficientes
 tablalabelf1$eficiencia = as.factor(tablalabelf1$eficiencia)
 
 tablaff1 <- tablaf1[which(tablaf1$eficiencia==1),] 
@@ -177,7 +261,7 @@ fronteraJC <- ggplot(data=tablalabelf1,
                     axis.line = element_line(colour = "black")
                     )+
               
-              ggtitle("Eficiencia técnica proceso reparación directa entre circuito (juzgados)"
+              ggtitle("Eficiencia técnica proceso reparación directa entre circuitos (juzgados)"
                       )+
               labs(x="Número de jueces",
                    y="Carga de trabajo (Demanda+Inventario)"
@@ -189,6 +273,8 @@ fronteraJC <- ggplot(data=tablalabelf1,
                               )
 fronteraJC
 
+# Guardar .png en directorio actual
+ggsave("frontera1.png",fronteraJC,dpi = 700, bg= "white")
 
 
 ###########################################
@@ -196,16 +282,16 @@ fronteraJC
 ###########################################
 
 ########### Gráfico con función del paquete para saber forma de la frontera
-input_3 <- as.matrix(input_3)
-output_3 <- as.matrix(output_3)
-plotef3 <- dea.plot.frontier(input_3,output_3,RTS = "vrs")
+#inputs3 <- as.matrix(inputs3)
+#outputs3 <- as.matrix(outputs3)
+#plotef3 <- dea.plot.frontier(inputs3,outputs3,RTS = "vrs")
 ###########
 
 tablaf3 <- data.frame(tribdist,eficiencia=dea3$eff)
 tablalabelf3 <- data.frame(tribdist,eficiencia=dea3$eff)
 tablalabelf3$eficiencia[-which(tablalabelf3$eficiencia==1)]=0  
 tablalabelf3$color=rep("firebrick4",length(tablalabelf3$DISTRITO))
-tablalabelf3$color[which(tablalabelf3$eficiencia==1)]=rep("slateblue4",3)
+tablalabelf3$color[which(tablalabelf3$eficiencia==1)]=rep("slateblue4",3) #número de unidades eficientes
 tablalabelf3$eficiencia = as.factor(tablalabelf3$eficiencia)
 
 tablaff3 <- tablaf3[which(tablaf3$eficiencia==1),] 
@@ -230,7 +316,11 @@ fronteraTD <- ggplot(data=tablalabelf3,
                                 )+
               theme(legend.position = "none",
                     rect=element_rect(fill = "transparent"),
-                    plot.title = element_text(hjust = 0.5)
+                    plot.title = element_text(hjust = 0.5),
+                    # para fondo blanco
+                    panel.grid=element_blank(),
+                    panel.background = element_blank(),
+                    axis.line = element_line(colour = "black")
                     )+
               ggtitle("Eficiencia técnica proceso reparación directa entre distritos para tribunales"
                       )+
@@ -244,43 +334,9 @@ fronteraTD <- ggplot(data=tablalabelf3,
                               )
 fronteraTD
 
-######################
-##### DATA CLOUD #####
-######################
+# Guardar .png en directorio actual
+ggsave("frontera2.png",fronteraTD,dpi = 700, bg= "white")
 
-# Datos atípicos para juzgados entre circuit
-x1 <- with(juzgcirc, cbind(juzgcirc$Jueces))
-y1 <- with(juzgcirc, cbind(juzgcirc$Carga))
-w1 <- with(juzgcirc, cbind(juzgcirc$Egresos))
-x1y1w1 <- cbind(x1,y1,w1)
-D1 <- det(t(x1y1w1)%*%x1y1w1)
-i1 <- c(1) # DMUs a quitarse, que se sospeche que sean atípicos
-x1y1w1i1 = x1y1w1[-i1,]
-D1i <- det( t(x1y1w1i1) %*% x1y1w1i1 )
-R1i <- D1i/D1
-R1i # Valores pequeños de Ri me indican presencia de datos atípicos
 
-# Datos atípicos para juzgados entre distritos
-x2 <- with(juzgdist, cbind(juzgdist$Jueces))
-y2 <- with(juzgdist, cbind(juzgdist$Carga))
-w2 <- with(juzgdist, cbind(juzgdist$Egresos))
-x2y2w2 <- cbind(x2,y2,w2)
-D2 <- det(t(x2y2w2)%*%x2y2w2)
-i2 <- c(1) # DMUs a quitarse, que se sospeche que sean atípicos
-x2y2w2i2 = x2y2w2[-i2,]
-D2i <- det( t(x2y2w2i2) %*% x2y2w2i2 )
-R2i <- D2i/D2
-R2i # Valores pequeños de Ri me indican presencia de datos atípicos
 
-# Datos atípicos para tribunales entre distritos
-x3 <- with(tribdist, cbind(tribdist$Jueces))
-y3 <- with(tribdist, cbind(tribdist$Carga))
-w3 <- with(tribdist, cbind(tribdist$Egresos))
-x3y3w3 <- cbind(x3,y3,w3)
-D3 <- det(t(x3y3w3)%*%x3y3w3)
-i3 <- c(1) # DMUs a quitarse, que se sospeche que sean atípicos
-x3y3w3i3 = x3y3w3[-i3,]
-D3i <- det( t(x3y3w3i3) %*% x3y3w3i3 )
-R3i <- D3i/D3
-R3i # Valores pequeños de Ri me indican presencia de datos atípicos
 
